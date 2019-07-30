b0f4dd549b3d8316f5785b08f918fb32be0dcf61
Neal <neal@webcheckout.net>
2019-07-30 15:06:02 -0500

    add event listener for enter and click

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
-  var x = 23;
-  var add = function(a){
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b){
-      return add(b);
-    }
-  }
+  
 })();
 
+// UI CONTROLLER
 var UIController = (function(){
   
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-  var z = budgetCtrl.publicTest(5);
+  
+  var ctrlAddItem = function(){
+    // get the field input data
 
-  return {
-    anotherPublic: function(){
-      console.log(z);
-    }
+    // add the item to the budget controller
+
+    // add the new item to the UI
+
+    // calculate the budget 
+
+    // display the budget on the UI
+    
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event){
+    switch(event.keyCode || event.which){
+      case 13:
+        ctrlAddItem();
+        break;
+      default:
+        break;
+    }
+  });
 
 })(budgetController, UIController);
